de0e741732212d36642b90be3df5c45e8871ef37
duduusxz <eduardoataidemelo@gmail.com>
2025-10-01 16:57:45 -0300

    .

---
+++ imagem_flutter/lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+final String _imgPath = 'assets/leao';
+int _imgIndex = 1;
+final String _imgExtension = '.png';
+String _img = 'assets/leao.png';
+
+
+  void _incrementCounter() {
+    setState(() {
+     
+      _imgIndex++;
+      _img = '$_imgPath$_imgIndex$_imgExtension';
+
+      if (_imgIndex > 10) {
+        _imgIndex = 1;
+        _img = '$_imgPath$_imgIndex$_imgExtension';
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+        Image.asset(
+             _img,
+             width: 400,
+             height: 200,
+             fit: BoxFit.cover,
+             ),
+
+
+            ElevatedButton(onPressed: _incrementCounter,
+style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 223, 222, 160),//fundo
+  foregroundColor: const Color.fromARGB(255, 30, 31, 28), // cor do texto
+  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+  shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(12), // bordas arredondadas
+),
+                 textStyle: const TextStyle(
+                 fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    ),
+                 ), child: const Text("Trocar Imagem")),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
